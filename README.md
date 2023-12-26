435b2bd45e08d525080b2f506987828eed1e3378
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-25 18:03:54 -0800

    Documentación README

--- README.md
+++ README.md
@@ -1,8 +1,77 @@
-# React + Vite
+# PROYECTO 5: Aplicación de Comercio Electrónico.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## **ÍNDICE**
 
-Currently, two official plugins are available:
+* [1. Introducción](#1-introduccion)
+* [2. Prototipo](#2-prototipo)
+* [3. Instalación](#3-instalación)
+* [4. GitHub](#4-github)
+* [5. Layout](#5-layout)
+* [6. API](#6-api)
+* [7. Base de Datos](#7-base-de-datos)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## 1. Intro
+
+El proyecto consiste en la elaboración de un sitio Web de comercio electrónico, utilizando tecnologías como: Express, MongoDB, JWT para el Backend. React, Axios, Bootstrap y GitHub Pages para el Frond-End. Que permitan al usuario visualizar los productos, ver su detalle y realizar compras a través de Mercado Pago. Además de tener la opción de registrarse en el sitio.
+
+## 2. Prototipo
+**Home**
+![Banner](./src/img/pro1.jpg)
+**Productos**
+![Banner](./src/img/pro2.jpg)
+
+## 3. Instalación
+
+### Frond-End Client:
+- npm install
+- npm install react-router-dom
+- npm install react-bootstrap bootstrap
+- npm install gh-pages
+- npm install nodemon
+- npm run dev
+- npm install axios
+- npm install mercadopago
+
+### Back-End Server:
+- npm init -y
+- npm install express
+- npm install dotenv
+- npm install nodemon
+- npm install cors
+- npm install bcrypt
+- npm install express-jwt
+- npm install jsonwebtoken
+- npm install moongose
+- npm install mongodb
+
+## 4. GitHub
+
+**Web Restaurante** <https://cristian-aliaga.github.io/preproyecto5_v1/>
+
+**Proyecto GitHub** <https://github.com/cristian-aliaga/preproyecto5_v1>
+
+## 5. Layout 
+
+1. Home: Página principal.
+2. Productos: Acceso a los productos de la tienda, con la opción de ver detalles y comprar.
+3. Mi Perfil: Información del perfil del usuario.
+4. Registro: Permite realizar el registro mediante correo electronico y contraseña.
+5. Login: Permite realizar el login para acceder a páginas que requieren autenticación.
+
+>Nota:
+>>Mi perfil y compras presentan problemas, los que están en corrección.
+
+## 6. API
+Se utilizó API render para la implementación de dos API, una para las consultas a la base de datos MongoDB Atlás, para el menú, usuarios y Blog. Mientras que la segunda API es para las transacciones de Mercado Pago.
+
+**API Render**
+![Render](./src/img/render.jpg)
+**Prueba de Transacción Mercado Pago**
+![Mercado Pago](./src/img/mercadopago.jpg)
+
+## 7. Base de Datos
+La aplicación almacenará los datos del Menú, reservas y Blog, en una Base de Datos NoSQL en Firebase. La base de dotos "Bootcamp" tiene tres colecciones; 
+- Categorias.
+- Productos.
+- Usuarios.
+![Prototipo](./src/img/mongodb.jpg)
--- src/components/MercadoPago.jsx
+++ src/components/MercadoPago.jsx
@@ -1,28 +1,110 @@
 import axios from "axios";
-import productos from "../productos.json";
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import { useState, useEffect } from "react";
+import React from 'react'
 
 export const MercadoPago = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          'https://preproyecto5.onrender.com/v1/product'
+        );
+        setData(response.data);
+        console.log(data)
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        console.log(err.message)
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  if (loading) {
+    return <div><h3>Cargando Productos...Primera consulta puede tomar entre 10 a 20 segundos.</h3></div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   const FuncionComprar = async (producto) => {
     const response = await axios.post(
-      "https://preproyecto5-mp.onrender.com/Mercado_Pago",
+      "http://localhost:4000/Mercado_Pago",
       producto
     );
-      console.log(producto)
-    window.location.href = response.data;
+    console.log(producto)
+    window.location.href = response.mdata;
   };
-  
-  return (
-    <div className="ContainerSuperior">
-      {productos.map((e) => (
-        <div className="ContainerProducto">
-          <p>{e.nombre}</p>
-          <p>{`$ ${e.precio}`}</p>
 
-          <button onClick={() => FuncionComprar(e)}>Comprar</button>
+  console.log(data.detail)
+  console.log(typeof data.detail)
+
+  const result = Object.values(data.detail);
+
+  console.log(result)
+  console.log(typeof result)
+
+  return (
+    <>
+      <div className="ContainerSuperior">
+        <div>
+          {
+            data.detail && (
+              <ul>
+                {data.detail.map(item => (
+                  <div className="ContainerProducto" key={item._id} >
+                    <p>{item.name}</p>
+                    <p>{`${item.price}`}</p>
+                    <button onClick={() => FuncionComprar(item)}>Comprar</button>
+                  </div>
+                ))}
+              </ul>
+            )
+          }
         </div>
-      ))}
-    </div>
+        {/* {
+          result.map(item => (
+            <div className="ContainerProducto" key={item._id} >
+              <p>{item.name}</p>
+              <p>{`$ ${item.price}`}</p>
+
+              <button onClick={() => FuncionComprar(item)}>Comprar</button>
+            </div>
+          ))
+        } */}
+      </div>
+      {/* {
+        data.detail.map(item => (
+          <div key={item._id} className="d-flex justify-content-center d-inline-flex p-2">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={`https://raw.githubusercontent.com/cristian-aliaga/preproyecto5_v1/main/puclic/images/${item.image}`} />
+              <Card.Body>
+                <Card.Title >{item.name}</Card.Title>
+                <Card.Text>
+                  {item.description}
+                </Card.Text>
+                <Card.Text>
+                  {item.price}
+                </Card.Text>
+                <Button className="px-3 me-2" variant="primary" onClick={() => FuncionComprar(item)}>Comprar</Button>
+                <Button href={`/preproyecto5_v1/#products/${item.name}`} className="px-3" variant="secondary">Ver Detalles</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))
+      } */}
+    </>
   );
 }
 
-export default MercadoPago;
+// export default MercadoPago;
